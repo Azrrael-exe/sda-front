1bc94a293a687b4c1b4c53ccd7726a997d0d4f61
Azrrael <vcamargo.e@gmail.com>
2017-06-07 23:09:00 -0500

    Grafica

--- utils/populate_reads.py
+++ utils/populate_reads.py
@@ -1,16 +1,16 @@
 import requests
 from random import random
-from json import dumps
+from datetime import datetime
 
 url = 'http://solarpes.herokuapp.com/client/'
 user = '3006750425'
 panel = 'mypanel'
 
 
-for i in xrange(100):
+for i in xrange(20):
     payload = {
+        'time' : datetime(2017,06,07,i,0,0).__str__(),
         'temperature': 20 + (20 * random()),
-        'radiation': random(),
         'current': {
             'network' : random(),
             'panel' : random(),
